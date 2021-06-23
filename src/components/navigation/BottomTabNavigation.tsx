854583e1d8e4357c74c76a4c20b1ffe02fad9734
Daniel <33708658+realcryptogates@users.noreply.github.com>
2021-06-24 03:32:37 +0900

    added nav

---
+++ src/components/navigation/BottomTabNavigation.tsx
@@ -0,0 +1,100 @@
+/**
+ * Learn more about createBottomTabNavigator:
+ * https://reactnavigation.org/docs/bottom-tab-navigator
+ */
+
+ import { Ionicons } from '@expo/vector-icons';
+ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+ import { createStackNavigator } from '@react-navigation/stack';
+ import * as React from 'react';
+ 
+ import Colors from './constants/Colors';
+ import useColorScheme from './hooks/useColorScheme';
+ import HomePage from '../pages/HomePage';
+ import RegisterPage from '../pages/RegisterPage';
+ import LoginPage from '../pages/LoginPage';
+ import { BottomTabParamList, TabOneParamList, TabTwoParamList, TabThreeParamList } from './Types';
+ 
+ const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+ 
+ export default function BottomTabNavigator() {
+   const colorScheme = useColorScheme();
+ 
+   return (
+     <BottomTab.Navigator
+       initialRouteName="Home"
+       tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
+       <BottomTab.Screen
+         name="Home"
+         component={TabOneNavigator}
+         options={{
+           tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+         }}
+       />
+       <BottomTab.Screen
+         name="Register"
+         component={TabTwoNavigator}
+         options={{
+           tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+         }}
+       />
+        <BottomTab.Screen
+         name="Login"
+         component={TabThreeNavigator}
+         options={{
+           tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+         }}
+       />
+     </BottomTab.Navigator>
+   );
+ }
+ 
+ // You can explore the built-in icon families and icons on the web at:
+ // https://icons.expo.fyi/
+ function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
+   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+ }
+ 
+ // Each tab has its own navigation stack, you can read more about this pattern here:
+ // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
+ const TabOneStack = createStackNavigator<TabOneParamList>();
+ 
+ function TabOneNavigator() {
+   return (
+     <TabOneStack.Navigator>
+       <TabOneStack.Screen
+         name="TabOneScreen"
+         component={HomePage}
+         options={{ headerTitle: 'Home' }}
+       />
+     </TabOneStack.Navigator>
+   );
+ }
+ 
+ const TabTwoStack = createStackNavigator<TabTwoParamList>();
+ 
+ function TabTwoNavigator() {
+   return (
+     <TabTwoStack.Navigator>
+       <TabTwoStack.Screen
+         name="TabTwoScreen"
+         component={RegisterPage}
+         options={{ headerTitle: 'Register' }}
+       />
+     </TabTwoStack.Navigator>
+   );
+ }
+
+ const TabThreeStack = createStackNavigator<TabThreeParamList>();
+ function TabThreeNavigator() {
+    return (
+      <TabThreeStack.Navigator>
+        <TabThreeStack.Screen
+          name="TabThreeScreen"
+          component={LoginPage}
+          options={{ headerTitle: 'Login' }}
+        />
+      </TabThreeStack.Navigator>
+    );
+  }
+ 
